d4b658c91877caa6f1d96d779987f05e04619afd
zhufengnodejs <zhufengnodejs@126.com>
2017-07-25 12:09:29 +0800

    编写配置文件

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -3,11 +3,22 @@ import './Slider.less'
 export default class Slider extends Component{
   constructor(){
     super();
+    this.state = {index:0};//索引值默认为0
+  }
+  componentDidMount(){
+    this.timer = setInterval(()=>{
+      this.setState({index:this.state.index+1})
+    },2000)//每隔2秒钟让index加1
   }
   render(){
+    let style = {//给ul增加自定义样式对象
+      width:this.props.images.length*300,//宽度是300*图片的数量
+      left:this.state.index*-300+'px',//距离左边
+      transitionDuration:'1s'//渐变的时间是1秒
+    }
     return (
       <div className="slider-wrapper">
-        <ul className="sliders">
+        <ul className="sliders" style={style}>
           {
             this.props.images.map((image,index)=>(
               <li className="slider" key={index}>
@@ -15,7 +26,6 @@ export default class Slider extends Component{
               </li>
             ))
           }
-
         </ul>
       </div>
     )
