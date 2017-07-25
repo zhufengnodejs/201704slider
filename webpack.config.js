bf023ea48c07cf15f2c6ce7d8d02e46ea78d1e0e
zhufengnodejs <zhufengnodejs@126.com>
2017-07-25 10:35:49 +0800

    编写配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   //打包的入口文件
   entry: path.resolve('app/index.js'),
@@ -19,6 +20,14 @@ module.exports = {
         exclude:/node_modules///为了提高解析速度，需要忽略掉node_modules下的东西
       }
     ]
-  }
+  },
+  //配置插件
+  plugins: [
+    //可以以index.html作为模板，并向其中插入打包后的bundle.js文件。然后保存到目标路径下
+    new HtmlWebpackPlugin({
+      //以app下的index.html作为模板
+      template:'./app/index.html'
+    })
+  ]
 
 }
