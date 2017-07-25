7f3a07098932f4625e128280d9e4190402e94a1c
zhufengnodejs <zhufengnodejs@126.com>
2017-07-25 15:40:17 +0800

    编写配置文件

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -1,42 +1,59 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './Slider.less'
 import SliderItems from "./SliderItems";
-export default class Slider extends Component{
-  constructor(){
+export default class Slider extends Component {
+  constructor() {
     super();
-    this.state = {index:0};//索引值默认为0
+    this.state = {index: 0};//索引值默认为0
   }
+
   //表示移动index step步长
-  turn = (step)=>{
+  turn = (step) => {
     //用当前的索引加上步长,得到新的值
     let index = this.state.index + step;
-    if(index>=this.props.images.length){
-      index = 0;
+    if (index > this.props.images.length) {
+      //先把转换的过渡时间设置为0
+      this.sliders.style.transitionDuration = '0s';
+      this.sliders.style.left = 0;//直接操作DOM元素，把left置为0
+      index = 1;
+      setTimeout(()=>{
+        this.sliders.style.transitionDuration = '1s';
+        this.setState({index});
+      },0);
+      return;
     }
     this.setState({index});
   }
   //开始自动轮播,鼠标移动上去之后暂停自动轮播。鼠标移走之后开启自动轮播
-  go = ()=>{
-    this.timer = setInterval(()=>{
+  go = () => {
+    this.timer = setInterval(() => {
       this.turn(1);
-    },2000)//每隔2秒钟让index加1
+    }, 2000)//每隔2秒钟让index加1
   }
-  componentDidMount(){
+
+  componentDidMount() {
     this.go();
   }
-  stop=()=>{
+  //把子组件中的SliderItems组件父组件
+  setSliders = (ref)=>{
+    this.sliders = ref;
+  }
+
+  stop = () => {
     clearInterval(this.timer);
   }
-  render(){
+
+  render() {
     return (
       <div className="slider-wrapper"
            onMouseOver={this.stop}
            onMouseOut={this.go}
       >
-      <SliderItems
-        images={this.props.images}
-        index={this.state.index}
-      />
+        <SliderItems
+          images={this.props.images}
+          index={this.state.index}
+          setSliders  = {this.setSliders}
+        />
       </div>
     )
   }
--- app/components/SliderItems.js
+++ app/components/SliderItems.js
@@ -1,19 +1,25 @@
 import React,{Component} from 'react';
 import SliderItem from "./SliderItem";
 export default class SliderItems extends Component{
+  //在组件挂载完成后才能真正得到DOM元素
+  componentDidMount(){
+    this.props.setSliders(this.refs.sliders);
+  }
   render(){
     let style = {//给ul增加自定义样式对象
-      width:this.props.images.length*300,//宽度是300*图片的数量
+      width:(this.props.images.length+1)*300+'px',//宽度是300*图片的数量+1
       left:this.props.index*-300+'px',//距离左边
       transitionDuration:'1s'//渐变的时间是1秒
     }
+
     return (
-      <ul className="sliders" style={style}>
+      <ul ref="sliders" className="sliders" style={style}>
         {
           this.props.images.map((image,index)=>(
             <SliderItem key={index} image={image}/>
           ))
         }
+        <SliderItem key={this.props.images.length} image={this.props.images[0]}/>
       </ul>
     )
   }
