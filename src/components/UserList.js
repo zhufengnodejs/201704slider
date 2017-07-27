2b16db5e725afe6030928dc941704b20e9a91a17
zhufengnodejs <zhufengnodejs@126.com>
2017-07-27 15:25:45 +0800

    编写配置文件

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,8 +1,33 @@
-import React,{Component} from 'react';
-export default class UserList extends Component{
- render(){
-   return (
-     <div>用户列表</div>
-   )
- }
+import React, {Component} from 'react';
+export default class UserList extends Component {
+  constructor() {
+    super();
+    //定义一个初始的状态对象
+    this.state  = {users:[]};
+  }
+  componentDidMount() {//组件加载完成
+
+  }
+  render() {
+    return (
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>用户名</th>
+            <th>邮箱</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          this.state.users.map((user,index)=>(
+            <tr key={index}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    )
+  }
 }
