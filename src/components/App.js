137f37695b6890ad011d9953588b086d76f535b6
zhufengnodejs <zhufengnodejs@126.com>
2017-07-27 10:34:47 +0800

    编写配置文件

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+// http://www.baidu.com#/app
+//#一般用来代表的是锚点，指向页面中的某个位置
 import {
-  Router,Route,Link
+  HashRouter as Router, Route, Link
 } from 'react-router-dom';
-export default class App extends Component{
-  render(){
+function Home(){
+  return <div>Home</div>
+}
+let User = ()=><div>User</div>
+export default class App extends Component {
+  render() {
     //react元素用来指明此组件是如何渲染的
     return (
-      <div>
-        App
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><a href="#/">首页</a></li>
+            <li><a href="#/user">用户管理</a></li>
+          </ul>
+          <hr/>
+          <Route path="/" component={Home}/>
+          <Route path="/user" component={User}/>
+        </div>
+      </Router>
     )
   }
 }
