354f13f8121fc3f13fc734a17c11c5ca7469e393
zhufengnodejs <zhufengnodejs@126.com>
2017-08-01 10:55:52 +0800

    编写配置文件

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
+/*
 //纯函数
 //1. 固定的输入产生固定的输出
 //2. 并且不改变输入参数
@@ -5,3 +6,8 @@ let sum = (a,b)=>a+b+Math.random();
 //不能改参数的任何属性
 let grow = obj =>obj.age+1;
 
+*/
+// null un
+let a=null;
+//Cannot read property 'type' of undefined
+console.log(a.type);
--- learn-redux/redux.js
+++ learn-redux/redux.js
@@ -1,25 +1,26 @@
 //这是一个方法，用于创建一个仓库
 //reducer每个项目都不一样，所以需要作为参数参数传进来,reducer用于从老状态和action得到新状态
-let createStore = (reducer)=>{
-   let state;//初始的状态
-    //用于返回最新的状态
-   let getState = () => state;
-   let listeners = [];//存放着所有的监听函数
-   //提供给外界调用，用来派发action的
-   let dispatch = (action)=>{
-      //调用reducer,经过计算，返回新状态
-     state = reducer(state,action);
-     //把监听数组中的每个函数都执行一次
-     listeners.forEach(listener=>listener());
-   }
-   //订阅状态变化事件监听
-   let subscribe = listener => listeners.push(listener);
-   //返回一个仓库对象
-   return {
-     getState,
-     dispatch,
-     subscribe
-   }
+export function createStore(reducer) {
+  let state;//初始的状态
+  //用于返回最新的状态
+  let getState = () => state;
+  let listeners = [];//存放着所有的监听函数
+  //提供给外界调用，用来派发action的
+  let dispatch = (action) => {
+    //调用reducer,经过计算，返回新状态
+    state = reducer(state, action);
+    //把监听数组中的每个函数都执行一次
+    listeners.forEach(listener => listener());
+  }
+  //订阅状态变化事件监听
+  let subscribe = listener => listeners.push(listener);
+  //直接派发一个undefined action
+  dispatch();
+  //返回一个仓库对象
+  return {
+    getState,
+    dispatch,
+    subscribe
+  }
 }
 //默认导出一个对象，createStore是它的一个属性
-export default {createStore}
