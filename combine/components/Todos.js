37f8ba90102738e14f813985dd30e42103b69dca
zhufengnodejs <zhufengnodejs@126.com>
2017-08-01 17:41:43 +0800

    combinereducer

--- combine/components/Todos.js
+++ combine/components/Todos.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import store from '../store';
 export default class Todos extends React.Component{
+  constructor(){
+    super();
+    this.state = {list:store.getState().todos.list};
+  }
   render(){
     return (
       <div>
-        Todos
+        <input type="text" onKeyDown={this.handleKeyDown}/>
+        <ul>
+          {
+            this.state.list.map((item,index)=>(
+              <li key={index}>{item}</li>
+            ))
+          }
+        </ul>
       </div>
     )
   }
