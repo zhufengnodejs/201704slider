aa71bc7a9ed1e6377d28b896a1a7daa93a2039db
zhufengnodejs <zhufengnodejs@126.com>
2017-08-01 16:44:32 +0800

    combinereducer

--- 1.js
+++ 1.js
@@ -1,13 +1,4 @@
-/*
-//纯函数
-//1. 固定的输入产生固定的输出
-//2. 并且不改变输入参数
-let sum = (a,b)=>a+b+Math.random();
-//不能改参数的任何属性
-let grow = obj =>obj.age+1;
-
-*/
-// null un
-let a=null;
-//Cannot read property 'type' of undefined
-console.log(a.type);
+let arr = [1,2];
+//let arr2 = [...arr,3];
+arr.push(3);
+console.log(arr);
--- combine/reducers/index.js
+++ combine/reducers/index.js
@@ -1,3 +1,22 @@
-/**
- * Created by Administrator on 2017/8/1.
- */
+import counter from './counter';
+import todos from './todos';
+import {combineReducers} from 'redux';
+//把两个reducer合并成一个reducer
+let reducer = combineReducers({
+  a:counter,
+  b:todos
+});
+
+function reducer(state,action){
+  //这是最终的返回的合并后的状态树
+  let combinedState = {};
+  combinedState.a = counter(state.a,action);//{number:1}
+  combinedState.b = todos(state.b,action);//{list:[]}
+  return combinedState;
+  // {counter:{number:1},todos:{list:[]}}
+  //
+}
+
+
+
+export default reducer;
