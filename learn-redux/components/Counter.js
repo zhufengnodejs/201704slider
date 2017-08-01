bf5c78dfbf5bf50e1c043a8636198552fe0cf1e6
zhufengnodejs <zhufengnodejs@126.com>
2017-08-01 11:44:43 +0800

    编写配置文件

---
+++ learn-redux/components/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default class Counter extends React.Component{
+  render(){
+    return (
+      <div>
+        <p></p>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    )
+  }
+}
--- learn-redux/index.js
+++ learn-redux/index.js
@@ -2,3 +2,8 @@
  * 1.创建一个Counter组件并导出
  * 2.在index.js里导入Counter组件并渲染到页面中
  **/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from "./components/Counter";
+
+ReactDOM.render(<Counter/>,document.querySelector('#root'));
