cf2159892ccf1d4a36a316d1eb2cd829a4f40289
zhufengnodejs <zhufengnodejs@126.com>
2017-08-01 10:38:47 +0800

    编写配置文件

--- 1.js
+++ 1.js
@@ -1,6 +1,7 @@
-let s = {name:'zfpx'};
-let str = JSON.stringify(s);
-let s2 = JSON.parse(str);
-console.log(s);
-console.log(s2);
-console.log(s===s2);
+//纯函数
+//1. 固定的输入产生固定的输出
+//2. 并且不改变输入参数
+let sum = (a,b)=>a+b+Math.random();
+//不能改参数的任何属性
+let grow = obj =>obj.age+1;
+
---
+++ learn-redux/index.js
@@ -0,0 +1,18 @@
+import {createStore} from './redux';
+//state就是状态对象，可以是数字，对象，数组等任意数据类型
+//不要在reducer里面修改参数的值，读取数据库的内容
+//一定要返回一个新的对象
+let reducer = (state={number:0},action)=>{
+  //判断动作的类型
+  switch (action.type){
+    //如果加1的话，返回老状态+1
+    case 'ADD':
+      return {number:state.number+1};
+    case 'SUB':
+      return {number:state.number-1};
+    default://如果是其它动作，则忽略不执行任何操作，返回老状态
+      return state;
+  }
+}
+let store = createStore(reducer);
+console.log(store.getState());
