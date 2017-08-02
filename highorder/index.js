ba5f28d36e233dc4f2dc7fec0bd025b45352daa5
zhufengnodejs <zhufengnodejs@126.com>
2017-08-02 12:37:18 +0800

    combinereducer

--- highorder/index.js
+++ highorder/index.js
@@ -1,40 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//老组件
-let Post = (props) => (
-  <div>
-    <p>{props.content}</p>
-    <button onClick={props.refresh}>刷新</button>
-  </div>
-)
-function getData(url) {
-  //获取URL地址中的内容，返回一个promise对象.then
-  return fetch(url).then((res) => res.json());
-}
-//接收URL地址作为参数
-let loadAndRefresh = url => OldComponent => {
-  class NewComponent extends React.Component {
-    constructor() {
-      super();//定义一个默认状态
-      this.state = {content: '数据加载中...'};
-    }//读取远程URL接口，拿到响应的数据，修改state
-    componentDidMount() {
-      this.refresh();//组件第一次加载的时候调用refresh方法
-    }
+import App from "./components/App";
 
-    refresh = () => {
-      this.setState({content: '数据加载中...'});
-      getData(url).then(data => this.setState({
-        content: data.time
-      }));
-    }
-
-    render() {
-      return <OldComponent refresh={this.refresh} content={this.state.content}/>
-    }
-  }
-  return NewComponent;
-}
-
-let NewPost = loadAndRefresh('http://localhost:3000/clock')(Post);
-ReactDOM.render(<NewPost/>, document.querySelector('#root'));
+ReactDOM.render(<App/>,document.querySelector('#root'));
--- highorder/server.js
+++ highorder/server.js
@@ -3,7 +3,13 @@ let app = express();
 app.get('/clock',function(req,res){
   res.setHeader('Access-Control-Allow-Origin','*')
   setTimeout(()=>{
-    res.send({time:new Date().toLocaleString()});
-  },3000);
+    res.send({time:'clock1:'+new Date().toLocaleString()});
+  },1000);
+});
+app.get('/clock2',function(req,res){
+  res.setHeader('Access-Control-Allow-Origin','*')
+  setTimeout(()=>{
+    res.send({time:'clock2:'+new Date().toLocaleString()});
+  },1000);
 });
 app.listen(3000);
