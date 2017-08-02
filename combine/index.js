568f7181afb6cdbc394d4afcee0565da69489c7c
zhufengnodejs <zhufengnodejs@126.com>
2017-08-02 16:58:26 +0800

    combinereducer

--- combine/index.js
+++ combine/index.js
@@ -5,7 +5,7 @@ import Todos from "./components/Todos";
 import store from './store';
 //这个react-redux库可以帮助我们自动连接
 //Provider 提供者,接收一个我们提供的store属性，然后又传递给了它的所有的子组件
-import {Provider} from 'react-redux';
+import {Provider} from './react-redux';
 ReactDOM.render(
   <Provider store={store}>
     <div>
--- combine/react-redux.js
+++ combine/react-redux.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 //连接仓库中的状态和dispatch方法和老组件 ,连接仓库(store)和老组件
 let connect = (mapStateToProps,mapDispatchToProps) => OldComponent => {
    class NewComponent extends React.Component{
      //从上下文中拿到store
     static contextTypes  = {
       store:React.PropTypes.object
-    }
-    constructor(){
-      super();
     }
     componentDidMount(){
       this.setState(mapStateToProps(this.context.store.getState()));
@@ -25,9 +23,21 @@ let connect = (mapStateToProps,mapDispatchToProps) => OldComponent => {
    }
    return NewComponent;
 } ;
+//提供者 向子组件提供store
+class Provider extends React.Component{
+  //规定了子组件上下文对象的属性名和属性的类型
+  static childContextTypes = {
+    store:PropTypes.object
+  }
+  getChildContext(){
+    return {store:this.props.store}
+  }
+  render(){
+    return (
+      //props.children指代的是子组件
+      this.props.children
+    )
+  }
+}
 
-
-
-
-
-export {connect}
+export {connect,Provider}
